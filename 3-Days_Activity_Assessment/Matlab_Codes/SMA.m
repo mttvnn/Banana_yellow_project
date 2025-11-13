9d6eb7b80e053989872c4513e52bf86085f3f9dd
Giacomo-eng <giacomo.facchini@studio.unibo.it>
2025-11-13 17:50:29 +0100

    Delete 3-Days_Activity_Assessment directory

--- 3-Days_Activity_Assessment/Matlab_Codes/SMA.m
+++
@@ -1,7 +0,0 @@
-function [SMA] = SMA(acceleration,time,fs)
-% Applying the SMA formulas
-SMA = [];
-for i=1:fs:length(time)-fs
-    SMA = [SMA sum(sum(abs(acceleration(i:(i+fs),:))))/fs];
-end
-
--- 3-Days_Activity_Assessment/Matlab_Codes/frequencyenergy.m
+++
@@ -1,6 +0,0 @@
-function [energy] = frequencyenergy(acel,time,fs)
-% Calculating the energy per seconds of the signals 
-energy = [];
-for i=1:fs:length(time)-fs
-    energy = [energy bandpower(acel(i:(fs+i),1),fs,[0 5])];
-end
